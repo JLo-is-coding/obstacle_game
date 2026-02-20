95280a49fcd1cc1e2fe9e2f094ff920532665c53
jlo2001 <Jacklowes2001@gmail.com>
2026-02-20 18:05:35 +0000

    Add player movement

--- main.py
+++ main.py
@@ -1,26 +1,60 @@
 import pygame
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT
+)
+
+screen_width = 1200
+screen_height = 700
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 75))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+    def move(self, pressed_keys):
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, +5)
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(+5, 0)
+
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > screen_width:
+            self.rect.right = screen_width
 
 def main():
     pygame.init()
 
-    screen_width = 1200
-    screen_height = 700
     screen = pygame.display.set_mode([screen_width, screen_height])
-    surf = pygame.Surface((50, 50))
-    surf.fill((0, 0, 0))
-    surf_center = (
-        (screen_width-surf.get_width())/2, 
-        (screen_height-surf.get_height())/2
-        )
-
+    player = Player()
+    
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         
+        pressed_keys = pygame.key.get_pressed()
+        player.move(pressed_keys)
+
         screen.fill((25, 255, 255))
-        screen.blit(surf, surf_center)
+        screen.blit(player.surf, player.rect)
         pygame.display.flip()
 
 if __name__ == "__main__":
