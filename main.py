209363c20530ad9ededcbeca293cd72526dfc767
jlo2001 <Jacklowes2001@gmail.com>
2026-02-20 19:07:55 +0000

    add enemy class and movement

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ from pygame.locals import (
     KEYDOWN,
     QUIT
 )
+import random
 
+# --- Constants ---
 screen_width = 1200
 screen_height = 700
 
+# --- Sprite Classes ---
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
@@ -19,6 +22,9 @@ class Player(pygame.sprite.Sprite):
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
 
+    def update(self, pressed_keys):
+        self.move(pressed_keys)
+    
     def move(self, pressed_keys):
         if pressed_keys[K_DOWN]:
             self.rect.move_ip(0, +5)
@@ -38,12 +44,38 @@ class Player(pygame.sprite.Sprite):
         if self.rect.right > screen_width:
             self.rect.right = screen_width
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.Surface((20, 10))
+        self.surf.fill((255, 0, 0))
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(screen_width +20, screen_width +100),
+                random.randint(0, screen_height)
+            )
+        )
+        self.speed = random.randint(5, 20)
+
+    def update(self):
+        self.move()
+        if self.rect.right < 0:
+            self.kill()
+    
+    def move(self):
+        self.rect.move_ip(-self.speed, 0)
+
+# --- Main Game Function ---
 def main():
+    # --- Initialising Pygame ---
     pygame.init()
 
+    # --- Object Initialisation ---
     screen = pygame.display.set_mode([screen_width, screen_height])
     player = Player()
+    enemy = Enemy()
     
+    # --- Main Game Loop ---
     running = True
     while running:
         for event in pygame.event.get():
@@ -51,11 +83,14 @@ def main():
                 running = False
         
         pressed_keys = pygame.key.get_pressed()
-        player.move(pressed_keys)
+        player.update(pressed_keys)
+        enemy.update()
 
         screen.fill((25, 255, 255))
         screen.blit(player.surf, player.rect)
+        screen.blit(enemy.surf, enemy.rect)
         pygame.display.flip()
 
+# --- Call For Main ---
 if __name__ == "__main__":
     main()
